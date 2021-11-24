4031348913139dcf258c3b7b44e50081333334bc
Sergejs Smirnovs <sergey@nbi.me>
2021-11-24 08:48:57 +0100

    ios 15

--- Package.swift
+++ Package.swift
@@ -6,10 +6,7 @@ import PackageDescription
 let package = Package(
     name: "Storable",
     platforms: [
-        .macOS(.v12),
-        .iOS(.v13),
-        .watchOS(.v6),
-        .tvOS(.v13),
+        .iOS(.v15),
     ],
     products: [
         .library(
